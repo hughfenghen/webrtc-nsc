c5550cd079671cc5445b938161c9a9ee102d6d06
hughfenghen <hughfenghen@gmail.com>
2019-12-29 17:43:55 +0800

    完成连接

--- src/host.js
+++ src/host.js
@@ -1,40 +1,64 @@
-const rtcPC = new RTCPeerConnection({
-  'iceServers': [
-    {
-      'urls': 'stun:stun.l.google.com:19302'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=udp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=tcp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    }
-  ]
-});
-rtcPC.onicecandidate = ({ candidate }) => {
-  console.log('------ send candiate to ', candidate);
-  if (candidate !== null) {
-    // sendCandidate(candidate);
+;(async () => {
+  const rtcPC = new RTCPeerConnection({
+    'iceServers': [
+      {
+        'urls': 'stun:stun.l.google.com:19302'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=udp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=tcp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      }
+    ]
+  });
+  
+  const channel = rtcPC.createDataChannel('sendDataChannel');
+  channel.onopen = () => {
+    console.log('------ channel open');
+    channel.send('dididi');
+  };
+  channel.onmessage = evt => {
+    console.log('------ channel onmessage', evt);
+  };
+
+  // ------------------------------------
+  const baseLink = `${location.origin}/invitee.html`
+  let linkParams = {
+    offer: null,
+    candidates: [],
   }
-};
-const channel = rtcPC.createDataChannel('sendDataChannel');
-channel.onopen = () => {
-  console.log('------ channel open');
-  channel.send('dididi');
-};
-channel.onmessage = evt => {
-  console.log('------ channel onmessage', evt); 
-};
+  
+  function updateLink() {
+    const link = baseLink + '?info=' + encodeURIComponent(JSON.stringify(linkParams))
+    const el = document.getElementById('invite-link')
+    el.innerHTML = link
+    el.setAttribute('href', link)
+  }
 
-const offer = await rtcPC.createOffer();
-rtcPC.setLocalDescription(offer);
+  rtcPC.onicecandidate = ({ candidate }) => {
+    console.log('------ send candiate to ', candidate);
+    if (candidate !== null) {
+      linkParams.candidates.push(candidate)
+      updateLink()
+      // sendCandidate(candidate);
+    }
+  };
+
+  const offer = await rtcPC.createOffer();
+  rtcPC.setLocalDescription(offer);
+  linkParams.offer = offer
+  updateLink()
+  console.log(1111, offer);
 
 
-document.getElementById('create-link')
-  .addEventListener('click', () => {
-    
-  })
+  document.getElementById('connect')
+    .addEventListener('click', () => {
+      const answer = JSON.parse(document.getElementById('answer').value);
+      rtcPC.setRemoteDescription(new RTCSessionDescription(answer));
+    })
+})();
--- src/invitee.js
+++ src/invitee.js
@@ -1,25 +1,46 @@
-const rtcPC = new RTCPeerConnection({
-  'iceServers': [
-    {
-      'urls': 'stun:stun.l.google.com:19302'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=udp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=tcp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    }
-  ]
-});
-// todo: parse url, get offer
-const offer = 'xxx'
+;(async () => {
+  const rtcPC = new RTCPeerConnection({
+    'iceServers': [
+      {
+        'urls': 'stun:stun.l.google.com:19302'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=udp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=tcp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      }
+    ]
+  });
+  const url = new URL(location.href)
+  const info = JSON.parse(url.searchParams.get('info'))
 
-rtcPC.setRemoteDescription(new RTCSessionDescription(offer));
-const answer = await rtcPC.createAnswer();
-rtcPC.setLocalDescription(answer);
+  rtcPC.setRemoteDescription(new RTCSessionDescription(info.offer));
+  const answer = await rtcPC.createAnswer();
+  rtcPC.setLocalDescription(answer);
+  document.getElementById('answer').innerHTML = JSON.stringify(answer);
+  console.log(11111, rtcPC.ondatachannel, info, answer);
 
-// sendAnswer(answer);
+  info.candidates.forEach(candidate => {
+    rtcPC.addIceCandidate(new RTCIceCandidate(candidate))
+  })
+
+  rtcPC.ondatachannel = (chanEvt) => {
+    console.log('------- ondatachannel');
+    const { channel } = chanEvt;
+    channel.onopen = () => {
+      console.log('------- channel open');
+    };
+    channel.onmessage = ({ data }) => {
+      console.log('===== onmessage: ', data);
+    };
+    channel.onclose = () => {
+      console.log('------ chan closed');
+      stopRecord();
+    };
+  }
+})();
