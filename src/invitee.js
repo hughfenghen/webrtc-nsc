52bf152bb36afee130729992ade2a0425fab4dc7
hughfenghen <hughfenghen@gmail.com>
2020-08-17 11:35:17 +0800

    更新stun 地址

--- src/invitee.js
+++ src/invitee.js
@@ -2,20 +2,9 @@ import chat from './chat/index.js'
 
 ;(async () => {
   const rtcPC = new RTCPeerConnection({
-    'iceServers': [
-      {
-        'urls': 'stun:stun.l.google.com:19302'
-      },
-      {
-        'urls': 'turn:192.158.29.39:3478?transport=udp',
-        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        'username': '28224511:1379330808'
-      },
-      {
-        'urls': 'turn:192.158.29.39:3478?transport=tcp',
-        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        'username': '28224511:1379330808'
-      }
+    iceServers: [
+      // { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:ks-sh-live-p2p-01.chat.bilibili.com:3478' },
     ]
   });
 
